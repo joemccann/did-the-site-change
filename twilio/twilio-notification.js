6d4a8038ae33b37f37b797b16458e1c9e0b271bf
Joe McCann <joseph.isaac@gmail.com>
2016-02-08 10:24:23 -0800

    Fixed formatting of message.

--- twilio/twilio-notification.js
+++ twilio/twilio-notification.js
@@ -5,14 +5,7 @@ const twilioClient = require('./twilio-client')
 	,	admins = require('../config/administrators.json')
 
 function formatMessage(errorToReport) {
-  return	'ALERT! It appears the server is' 
-  			+	' having issues. Exception: ' + errorToReport
-};
-
-
-module.exports.sendSMSNotification = function(message){
-  admins.forEach(function(admin){
-		console.log(admin.phoneNumber)
-    twilioClient.sendSMS(admin.phoneNumber, formatMessage(message))
-  })
+  return	'ALERT! ' + errorToReport
 }
+
+module.exports.sendSMSNotification = function(message){admins.forEach(function(admin){twilioClient.sendSMS(admin.phoneNumber, formatMessage(message))})}
